960d13df50ec187f18ec1b2593522787e178125f
rachitchaurasia17-ctrl <rachitchaurasia17@gmail.com>
2026-06-11 22:35:51 +0530

    Fix: replace all template addresses with client address

--- components/PropertyAI.jsx
+++ components/PropertyAI.jsx
@@ -11,7 +11,7 @@ const SYSTEM_PROMPT = `You are Property AI, the digital concierge for KBD Landco
 
 Firm facts:
 - Firm: KBD Landcorp
-- Office: SCO 1, 2nd Floor, Sector 79, Sahibzada Ajit Singh Nagar, Punjab 140308
+- Office: KBD Plaza, Sector 117, Airport Road Corridor, Mohali, Punjab 140301
 - Phone: +91-00000-00000
 - GMADA empanelled, RERA registered (PB-RERA)
 - Practice: Residential, Commercial, Land/Plots, Investment Advisory, NRI Services, Title & Due Diligence
